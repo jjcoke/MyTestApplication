19ced8a10b85d3b8c8731b28b7b45432e96ebbcd
Juraj <jjarmek@coca-cola.com>
2022-03-23 11:01:23 +0000

    testPractice

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,5 +9,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //test
         //another test and tool windows
+
+        // todo some comment
+
+        // todo change the folder structure and play around with it
+        // keymap shortcuts
+        // find files etc
+
     }
 }
